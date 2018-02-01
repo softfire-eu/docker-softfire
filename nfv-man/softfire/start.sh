f33a0fdf67128eaef0c66a1bcdbdb38f0cea1c2a
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2018-02-01 12:07:01 +0100

    first structure

--- nfv-man/softfire/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export myIp=`hostname -I`
-echo "Detected my ip address: $myIp"
-# sed -i "s/nfvman/${myIp}/g" /etc/softfire/nfv-manager.ini
-# cat /etc/softfire/nfv-manager.ini | grep ip
-
-nfv-manager
